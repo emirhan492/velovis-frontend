e9c84afa2dbbbea05beeb787226a1789a7183a2d
emirhan492 <ertergaming088@gmail.com>
2025-12-19 13:49:17 +0300

    Update next.config.ts

--- velovis-frontend/next.config.ts
+++ velovis-frontend/next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'via.placeholder.com',
+        hostname: 'res.cloudinary.com',
         port: '',
         pathname: '/**',
       },
